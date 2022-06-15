50eec2455333c19c2aab52b9054229e887d118b1
user <user@DESKTOP-R6V8DII>
2022-06-15 08:49:03 +0300

    Add project files.

---
+++ Stack(Simpel Generic)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_Simpel_Generic_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Create a new stack
+                Stack<string> orders = new();
+                orders.Push("SO 001");
+                orders.Push("SO 002");
+                orders.Push("SO 003");
+                orders.Push("SO 004");
+                orders.Push("SO 005");
+                orders.Push("SO 006");
+                orders.Push("SO 007");
+
+                //Enumarate the orders without disturbing its contents
+                Console.WriteLine("Sales orders");
+
+                foreach (string order in orders)
+                {
+                   
+                    Console.WriteLine(order);
+                }
+
+                Console.WriteLine("\nPopping {0}", orders.Pop());
+                Console.WriteLine("Peek at next item to destack:{0}", orders.Peek());
+
+                //Create a copy of the stack using ToArray method
+                Stack<string> orders2 = new(orders.ToArray());
+                Console.WriteLine("Content of the first order batch are");
+
+                foreach (string order in orders2)
+                {
+                    Console.WriteLine(order);
+                }
+
+                //Clear oreder2 contents
+                Console.WriteLine("\nOrders2.clear()");
+                orders.Clear();
+
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
